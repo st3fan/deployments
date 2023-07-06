b22033f4b258be27295aea24d37c2efae24df71f
Stefan Arentz <stefan@arentz.ca>
2023-07-05 21:27:08 -0400

    Initial import

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/st3fan/deployments
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "embed"
+  "log"
+	"net/http"
+  "os"
+	"text/template"
+)
+
+//go:embed templates/*
+var resources embed.FS
+
+var t = template.Must(template.ParseFS(resources, "templates/*"))
+
+func main() {
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    data := map[string]string {
+      "Region": os.Getenv("FLY_REGION"),
+    }
+    t.ExecuteTemplate(w, "index.html.tmpl", data)
+  })
+
+  log.Println("Listening on", port)
+  log.Fatal(http.ListenAndServe(":"+port, nil))
+}
